cc7a6d5709f591d3333164332992dc9ddfcec28a
Hamza Ali Khan <drstoneeren@gmail.com>
2022-11-27 06:45:03 +0500

    changes in scorllbar

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
     );
   } else {
     return (
-      <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden scrollbar scrollbar-track-gray-400/20 scroll-smooth scrollbar-thumb-[#F7AB0A]/80 z-0">
+      <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden scrollbar-track-gray-400/20 scroll-smooth scrollbar-thin scrollbar-thumb-[#F7AB0A]/80 z-0">
         <Header />
 
         <section id="hero" className="snap-start">
